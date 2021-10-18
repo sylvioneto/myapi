309b9d918d46f4fef240e592077bca8f2475f497
sylvio pedroza <sylvio@google.com>
2021-10-18 17:58:28 -0300

    initial

--- app/app.py
+++ app/app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 def index():
     user_name = os.getenv("USER_NAME", "Not Found")
     return "Hello, {}!".format(user_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0')
+
