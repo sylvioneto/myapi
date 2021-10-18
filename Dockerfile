d870a1d72ec899950a41a93175917a4b28a2ce02
sylvio pedroza <sylvio@google.com>
2021-10-18 16:50:59 -0300

    initial

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.7
 
 WORKDIR /app
-ADD app.py .
+ADD /app/app.py .
 ADD requirements.txt .
 
 RUN pip install -r requirements.txt
--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    user_name = os.getenv("USER_NAME", "Not Found")
-    return "Hello, {}!".format(user_name)
